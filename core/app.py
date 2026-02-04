3bd648ed1c29924d197e76c64c548a270a8eba13
Jithin S <jithinyakkara@gmail.com>
2026-02-04 11:48:40 +0530

    Update app.py

--- core/app.py
+++ core/app.py
@@ -1,8 +1,10 @@
 
+import asyncio
 from omnicore.core.lifecycle import LifecycleManager
 from omnicore.core.container import ServiceContainer
 from omnicore.core.runtime import RuntimeContext
-import asyncio
+from omnicore.modules.registry import ModuleRegistry
+from omnicore.plugins.loader import PluginLoader
 
 class OmniApp:
     def __init__(self, name: str):
@@ -10,11 +12,18 @@ class OmniApp:
         self.container = ServiceContainer()
         self.lifecycle = LifecycleManager()
         self.runtime = RuntimeContext(name)
+        self.modules = ModuleRegistry()
+        self.plugins = PluginLoader()
 
-def start(self):
-    self.runtime.mark_start()
-    asyncio.run(self.lifecycle.start_all())
+    def add_module(self, module):
+        self.modules.register(module)
+        self.lifecycle.register(module)
 
-def stop(self):
-    asyncio.run(self.lifecycle.stop_all())
-    self.runtime.mark_stop()
+    def start(self):
+        self.runtime.mark_start()
+        self.plugins.load(self)
+        asyncio.run(self.lifecycle.start_all())
+
+    def stop(self):
+        asyncio.run(self.lifecycle.stop_all())
+        self.runtime.mark_stop()
